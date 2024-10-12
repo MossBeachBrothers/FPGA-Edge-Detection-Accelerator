de49800694cef58ed74992547e16e5b3f930dbb5
akhilendrastavros <96711561+akhilendrastavros@users.noreply.github.com>
2024-10-12 14:41:30 -0700

    local changes using ModelSim for work dir

--- RTL/edge_detector.sv
+++ RTL/edge_detector.sv
@@ -1,7 +1,4 @@
-module edge_detector #(
-    parameter IMAGE_HEIGHT=500,
-    parameter IMAGE_WIDTH=400
-)(
+module edge_detector (
     input wire clk,
     input wire reset_n
     input wire enable,
@@ -11,86 +8,17 @@ module edge_detector #(
 ); 
 
 
-//intermediate signals
-wire input_image
-wire kernel_matrix
-wire output_image
-wire gradient_matrix
-wire theta_matrix
-wire non_max_image_z
-wire threshold_result
-wire hysterisis_result 
-wire output_image 
+control_unit cu_inst ();
 
-//control signals
-wire gaussian_enable, filter_enable, suppress_enable, threshold_enable, hysterisis_enable
+gaussian_kernel gaussian_inst ();
 
+sobel_filter filter_inst ();
 
-//status signals
-wire gaussian_done, filter_done, suppress_done, threshold_done, hsyerisis_done
+max_suppression_unit suppression_inst();
 
-control_unit cu_inst (
-    .clk(),
-    .reset_n(),
-    .enable(),
-    .gaussian_enable(),
-    .gaussian_done(),
-    .filter_enable(),
-    .filter_done(),
-    .suppress_enable(),
-    .suppress_done(),
-    .threshold_enable(),
-    .threshold_done(),
-    .hysterisis_enable(),
-    .hysterisis_done()
-);
+double_threshold_unit threshold_inst();
 
-gaussian_kernel gaussian_inst (
-    .clk(clk),
-    .reset(),
-    .enable(),
-    .done(),
-    kernel_matrix()
-);
-
-sobel_filter filter_inst (
-    .clk(clk),
-    .enable(),
-    .image(),
-    .done(),
-    .G(),
-    .theta()
-);
-
-non_max_suppression #(
-    WIDTH=5,
-    HEIGHT=5
-) suppression_inst(
-    .clk(clk),
-    .enable(),
-    .G(),
-    .theta(),
-    .done(),
-    .Z()
-);
-
-double_threshold_unit threshold_inst(
-    .clk(),
-    .reset(),
-    .enable(),
-    .done(),
-    .Z(),
-    .res()
-);
-
-hysterisis_unit hysterisis_inst(
-    .clk(clk),
-    .reset(),
-    .enable(),
-    .done(),
-    .img(),
-    .res()
-);
+hysterisis_unit hysterisis_inst();
 
 peripheral input_peripheral_inst();
 
